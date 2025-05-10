0e76ad14774945fdb357bdf4dfba9d70cbf82b29
Aljoboyer <tanveeazmain@gmail.com>
2025-05-10 11:15:57 +0600

    user schema addedd

---
+++ flat_finder_server/src/models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String, 
+    default: null
+  },
+  role: {
+    type: String,
+    enum: ['buyer', 'seller'],
+    required: true
+  },
+  address: {
+    city: { type: String },
+    state: { type: String },
+    country: { type: String, default: 'Bangladesh' } // optional
+  },
+
+ // Seller-specific fields
+  nidNo: {
+    type: String,
+    required: function () {
+      return this.role === 'seller';
+    }
+  },
+  propertyName: {
+    type: String,
+    required: function () {
+      return this.role === 'seller';
+    }
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
