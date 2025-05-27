82b48e4270f57766d31eedbfd0ae9c3c9867b940
Aljoboyer <tanveeazmain@gmail.com>
2025-05-27 12:45:21 +0600

    seller profile details api added

--- flat_finder_server/src/modules/controllers/user_controllers/profile_controllers.js
+++ flat_finder_server/src/modules/controllers/user_controllers/profile_controllers.js
@@ -1,4 +1,6 @@
 const UserCollection = require("../../../models/user");
+const PropertyCollection = require("../../../models/property");
+const ReviewCollection = require("../../../models/review");
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 
@@ -19,7 +21,52 @@ const getSpecificUser = async (req, res) => {
   }
 };
 
+// Updating user Profile
+const updateProfileController = async (req, res) => {
+  try {
+ 
+    const { _id, ...rest} = req.body
+
+     const updatedProperty = await UserCollection.findByIdAndUpdate(
+      _id,
+      rest,
+      { new: true } 
+    );
+
+    res.status(200).json({
+      msg: "updated successfully"
+    });
+  } catch (error) {
+
+    res.status(500).json({ message: "Update Failed", error });
+  }
+};
+
+//Seller review , property data
+const sellerDetailsController = async (req, res) => {
+  try {
+ 
+    const { property, seller_id } = req.query;
+    let detailsData = []
+    if(property){
+       detailsData = await PropertyCollection.find({seller: seller_id})
+    }
+    else{
+       detailsData = await ReviewCollection.find({seller: seller_id})
+    }
+
+    res.status(200).json({
+      data: detailsData
+    });
+  } catch (error) {
+
+    res.status(500).json({ message: "Seller Details fetching failed", error });
+  }
+};
+
 module.exports = {
-  getSpecificUser
+  getSpecificUser,
+  updateProfileController,
+  sellerDetailsController
 };
   
--- flat_finder_server/src/modules/routers/user_route.js
+++ flat_finder_server/src/modules/routers/user_route.js
@@ -1,7 +1,11 @@
-const { getSpecificUser } = require("../controllers/user_controllers/profile_controllers");
+const verifyJWT = require("../../middlewares/authChecker");
+const { getSpecificUser, updateProfileController, sellerDetailsController } = require("../controllers/user_controllers/profile_controllers");
 
 const router = require("express").Router();
 
 router.get("/profile", getSpecificUser);
+router.put("/update-profile", verifyJWT(), updateProfileController);
+router.get("/seller-details", verifyJWT(), sellerDetailsController);
+
 
 module.exports = router;
