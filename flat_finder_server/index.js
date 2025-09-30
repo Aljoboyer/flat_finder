7100fc0a9c8090c3fb66fda44d0e32b33d19f309
Aljoboyer <tanveeazmain@gmail.com>
2025-09-30 19:42:29 +0600

    swagger removed

--- flat_finder_server/index.js
+++ flat_finder_server/index.js
@@ -4,8 +4,6 @@ const cors = require("cors");
 require("dotenv").config();
 const http = require("http");
 const port = process.env.PORT || 5000;
-const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output.json");
 
 app.use(cors())
 app.use(express.json()); // For JSON bodies
@@ -26,8 +24,6 @@ const mainRouter = require('./src/modules/routers/index');
 // Use the main router
 app.use('/api', mainRouter); 
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 app.get('/', (req, res) => {
   res.send('Flat Finder Server is connected!')
 })
@@ -35,5 +31,3 @@ app.get('/', (req, res) => {
 server.listen(port, () => {
   console.log(`Flat Finder running on port ${port}`);
 });
-
-module.exports = app;
